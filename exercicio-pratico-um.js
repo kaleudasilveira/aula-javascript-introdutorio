f74923a8a83d030f2676e25ec464206f9f387a7a
kaleudasilveira <kaleu_da-silveira@estudante.sesisenai.org.br>
2025-09-10 23:45:10 +0000

    Adiciona arquivo exercicio-pratico-um.js com informações pessoais e preferências

---
+++ exercicio-pratico-um.js
@@ -0,0 +1,8 @@
+let meuNome = "Kaléu da Silveira";
+let minhaIdade = 29;
+let minhaCidade = "Timbó";
+let gostaDoFlamengo = true
+
+console.log(`Perfil: ${meuNome}, ${minhaIdade} anos!`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log(`Você gosta do Flamengo? ${gostaDoFlamengo}`);
