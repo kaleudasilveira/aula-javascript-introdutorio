265917bf66b06a6f37a651d7a22ad7de86c3c686
kaleudasilveira <kaleu_da-silveira@estudante.sesisenai.org.br>
2025-09-10 23:54:12 +0000

    Adiciona arquivo condicionais.js com lógica de aprovação e reprovacão

---
+++ condicionais.js
@@ -0,0 +1,17 @@
+/*
+    se (nota >= 7) {
+    escreva("Aprovado")
+    }senao{
+    escreva("Reprovado")
+     
+    }
+     */
+let nota = 7.0;
+
+    if(nota >= 7.0 && nota <= 10){
+        console.log("Aprovado!");
+       } else { 
+        console.log("Reprovado!");
+    }
+
+    console.log(typeof nota)
